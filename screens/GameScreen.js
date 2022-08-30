220e4e98f63587e3d2500d3a2d6076642e3b1ad1
Yunus Emre Aksu <emre_aksu@live.com>
2022-08-30 12:44:44 +0300

    Added new logic into GameScreen and created Title component

---
+++ screens/GameScreen.js
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
+
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View>LOG ROUNDS</View>
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -24,6 +24,8 @@ function StartGameScreen() {
       );
       return;
     }
+
+    onPickNumber(chosenNumber);
   }
   return (
     <View style={styles.inputContainer}>
