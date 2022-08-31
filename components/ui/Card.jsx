f0590c9c659ad9b02bbc6c8ea1a8242439b37368
Yunus Emre Aksu <emre_aksu@live.com>
2022-08-31 10:38:28 +0300

    Improved game screen visuals

---
+++ components/ui/Card.jsx
@@ -0,0 +1,26 @@
+import { View, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+function Card({ children }) {
+  return <View style={styles.card}>{children}</View>;
+}
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 36,
+    marginHorizontal: 24,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4, // Android shadow
+    // IOS shadow:
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Alert } from "react-native";
+import {  View, StyleSheet, Alert } from "react-native";
 import NumberContainer from "../components/game/NumberContainer";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 
@@ -56,8 +58,8 @@ function GameScreen({ userNumber, onGameOver }) {
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <View>
-        <Text>Higher or lower?</Text>
+      <Card>
+        <InstructionText>Higher or lower?</InstructionText>
         <View>
           <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
             -
@@ -66,7 +68,7 @@ function GameScreen({ userNumber, onGameOver }) {
             +
           </PrimaryButton>
         </View>
-      </View>
+      </Card>
       <View>LOG ROUNDS</View>
     </View>
   );
